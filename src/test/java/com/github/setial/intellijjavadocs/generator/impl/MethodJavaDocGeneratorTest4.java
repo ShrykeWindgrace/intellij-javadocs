a9f1877db45e4efee441252a33a2b288eb84ae26
ShrykeWindgrace <shryke.windgrace@gmail.com>
2025-03-17 18:06:41 +0100

    WIP, would not assemble
    something about invalid source version 21

---
+++ src/test/java/com/github/setial/intellijjavadocs/generator/impl/MethodJavaDocGeneratorTest4.java
@@ -0,0 +1,108 @@
+package com.github.setial.intellijjavadocs.generator.impl;
+
+import com.github.setial.intellijjavadocs.configuration.JavaDocConfiguration;
+import com.github.setial.intellijjavadocs.model.JavaDoc;
+import com.github.setial.intellijjavadocs.model.settings.GeneralSettings;
+import com.github.setial.intellijjavadocs.model.settings.Level;
+import com.github.setial.intellijjavadocs.model.settings.Mode;
+import com.github.setial.intellijjavadocs.model.settings.Visibility;
+import com.google.common.collect.Sets;
+import com.intellij.ide.highlighter.JavaFileType;
+import com.intellij.psi.PsiElementFactory;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiFileFactory;
+import com.intellij.psi.PsiMethod;
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase4;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+public class MethodJavaDocGeneratorTest4 extends LightJavaCodeInsightFixtureTestCase4 {
+
+    private MethodJavaDocGenerator methodJavaDocGenerator;
+    private GeneralSettings generalSettings;
+    private PsiMethod publicGetMethod;
+    private PsiMethod protectedGetMethod;
+
+    @Before
+    public void setUp() {
+        methodJavaDocGenerator = new MethodJavaDocGenerator(getFixture().getProject());
+        PsiElementFactory elementFactory = PsiElementFactory.getInstance(getFixture().getProject());
+        JavaDocConfiguration settings = getFixture().getProject().getService(JavaDocConfiguration.class);
+        generalSettings = settings.getConfiguration().getGeneralSettings();
+        generalSettings.setMode(Mode.REPLACE);
+        PsiFile psiFile = PsiFileFactory.getInstance(getFixture().getProject()).createFileFromText(
+                "Test.java", JavaFileType.INSTANCE, "public class Test {}");
+        publicGetMethod = elementFactory.createMethodFromText("public String getParam(String param) {}", psiFile);
+        protectedGetMethod = elementFactory.createMethodFromText("protected String getParam(String param) {}", psiFile);
+    }
+
+    @Test
+    public void testGenerateJavaDoc() {
+        generalSettings.setLevels(
+                Sets.immutableEnumSet(Level.FIELD, Level.METHOD, Level.TYPE));
+        generalSettings.setVisibilities(
+                Sets.immutableEnumSet(Visibility.DEFAULT, Visibility.PRIVATE, Visibility.PROTECTED, Visibility.PUBLIC));
+
+        JavaDoc javaDoc = methodJavaDocGenerator.generateJavaDoc(publicGetMethod);
+        assertThat(javaDoc, notNullValue());
+    }
+
+    @Test
+    public void testGenerateJavaDoc_NoPublicVisibility() {
+        generalSettings.setLevels(
+                Sets.immutableEnumSet(Level.FIELD, Level.METHOD, Level.TYPE));
+        generalSettings.setVisibilities(
+                Sets.immutableEnumSet(Visibility.DEFAULT, Visibility.PRIVATE, Visibility.PROTECTED));
+
+        JavaDoc javaDoc = methodJavaDocGenerator.generateJavaDoc(publicGetMethod);
+        assertThat(javaDoc, nullValue());
+    }
+
+    @Test
+    public void testGenerateJavaDoc_NoMethodLevel() {
+        generalSettings.setLevels(
+                Sets.immutableEnumSet(Level.FIELD, Level.TYPE));
+        generalSettings.setVisibilities(
+                Sets.immutableEnumSet(Visibility.DEFAULT, Visibility.PRIVATE, Visibility.PROTECTED, Visibility.PUBLIC));
+
+        JavaDoc javaDoc = methodJavaDocGenerator.generateJavaDoc(publicGetMethod);
+        assertThat(javaDoc, nullValue());
+    }
+
+    @Test
+    public void testGenerateJavaDoc_NoMethodLevelAndPublicVisibility() {
+        generalSettings.setLevels(
+                Sets.immutableEnumSet(Level.FIELD, Level.TYPE));
+        generalSettings.setVisibilities(
+                Sets.immutableEnumSet(Visibility.DEFAULT, Visibility.PRIVATE, Visibility.PROTECTED));
+
+        JavaDoc javaDoc = methodJavaDocGenerator.generateJavaDoc(publicGetMethod);
+        assertThat(javaDoc, nullValue());
+    }
+
+    @Test
+    public void testGenerateJavaDocProtected() {
+        generalSettings.setLevels(
+                Sets.immutableEnumSet(Level.FIELD, Level.TYPE, Level.METHOD));
+        generalSettings.setVisibilities(
+                Sets.immutableEnumSet(Visibility.DEFAULT, Visibility.PRIVATE, Visibility.PROTECTED));
+
+        JavaDoc javaDoc = methodJavaDocGenerator.generateJavaDoc(protectedGetMethod);
+        assertThat(javaDoc, notNullValue());
+    }
+
+    @Test
+    public void testGenerateJavaDocProtected_NoProtectedVisibility() {
+        generalSettings.setLevels(
+                Sets.immutableEnumSet(Level.FIELD, Level.TYPE, Level.METHOD));
+        generalSettings.setVisibilities(
+                Sets.immutableEnumSet(Visibility.DEFAULT, Visibility.PRIVATE));
+
+        JavaDoc javaDoc = methodJavaDocGenerator.generateJavaDoc(protectedGetMethod);
+        assertThat(javaDoc, nullValue());
+    }
+}
